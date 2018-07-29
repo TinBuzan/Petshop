d61a892168cba15b8972f74cf8dcd593b1e79717
tinbuzancic <tinbuzancic@gmail.com>
2018-07-29 09:15:11 -0400

    init commit

---
+++ contracts/Adoption.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.24;
+
+contract Adoption {
+  address[16] public adopters;
+
+  function adopt(uint petID) public returns (uint) {
+    require(petID >= 0 && petID <= 15);
+
+    adopters[petID] = msg.sender;
+
+    return petID;
+  }
+
+  function getAdopters() public returns (address[16]) {
+    return adopters;
+  }
+}
